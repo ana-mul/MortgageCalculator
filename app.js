ebbe86ae98ac7008a430f01068ec6fbaad710d02
Ana Muller <anamullerpsi@gmail.com>
2024-11-14 16:08:47 -0300

    Mortgage calculator -onprocess-

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ const inputYears = document.querySelector(".input-years");
 const spanYears = document.querySelector(".years-span");
 const inputPercentage = document.querySelector(".input-percentage");
 const spanPercentage = document.querySelector(".percentage-span");
-const labelOption = document.querySelectorAll('.option'); //selecciono todas las labels
-
+const labelOption = document.querySelectorAll(".option"); //selecciono todas las labels
 
 inputCurrency.addEventListener("click", () => {
   spanCurrency.style.backgroundColor = "hsl(61, 70%, 52%)";
@@ -32,14 +31,51 @@ inputPercentage.addEventListener("blur", () => {
   spanPercentage.style.backgroundColor = "hsl(202, 86%, 94%)";
 });
 
+labelOption.forEach((label) => {
+  //bucle
+  label.addEventListener("click", () => {
+    labelOption.forEach((label) => label.classList.remove("selected")); //elimino la clase selected de todas las labels
 
-labelOption.forEach(label =>{ //bucle
-    label.addEventListener('click', () =>{
-        labelOption.forEach(label => label.classList.remove('selected')); //elimino la clase selected de todas las labels
-
-        label.classList.add('selected'); //para despues agregar la clase selected a la opcion que clickee
-    });
+    label.classList.add("selected"); //para despues agregar la clase selected a la opcion que clickee
+  });
 });
 
-
 //ERROR STATES
+
+const errorMsg = document.querySelectorAll(".error-msg");
+const containerInputE = document.querySelectorAll('.input-error');
+const errorSpan = document.querySelectorAll('.span-error');
+const inputError = document.querySelectorAll(".input-error");
+const form = document.querySelector(".form-container");
+
+const checkInput = () => {
+  let formValid = true;
+
+  inputError.forEach((inputEmpty, index) => {
+    if (inputEmpty.type !== 'radio') {
+      if (inputEmpty.value ===''){
+        errorMsg[index].style.display = "block";
+        containerInputE[index].style.border = '1px solid hsl(4, 69%, 50%)';
+        errorSpan[index].style.backgroundColor = 'hsl(4, 69%, 50%)';
+        errorSpan[index].style.color = 'hsl(0, 0%, 100%)';
+        errorSpan[index].style.height = '30px';
+        formValid = false;
+      } else {
+        errorMsg[index].style.display = "none";
+        containerInputE[index].style.border = '1px solid hsl(200, 26%, 54%)';
+        errorSpan[index].style.backgroundColor = 'hsl(202, 86%, 94%)';
+        errorSpan[index].style.color = 'hsl(200, 24%, 40%)';
+        errorSpan[index].style.height = '28px';
+      }
+  }});
+  return formValid;
+};
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  checkInput();
+});
+
+//RESULT
+
+
